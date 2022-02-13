7af0fe110ed1bf9960a9f78de95734f60d8f125c
luisdev20 <lescobedo.im@gmail.com>
2022-02-13 17:51:12 -0500

    Task 5 js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -59,54 +59,53 @@ $(document).ready(function () {
       }
     }
   });
-});
 
-$('button').click(function () {
-  $('.place > article').remove();
-  $.ajax({
-    type: 'POST',
-    data: JSON.stringify({ amenities: Object.keys(AmenitiesChecked) }),
-    url: 'http://0.0.0.0:5001/api/v1/places_search',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: data => {
-      for (const place of data) {
-        const template = `<article>
-    <div class="title">
-      <h2>${place.name}</h2>
-      <div class="price_by_night">
-    $${place.price_by_night}
+  $('button').click(function () {
+    $('.place > article').remove();
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify({ amenities: Object.keys(AmenitiesChecked) }),
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: data => {
+        for (const place of data) {
+          const template = `<article>
+      <div class="title">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">
+      $${place.price_by_night}
+          </div>
         </div>
+        <div class="information">
+          <div class="max_guest">
+          <div class="image_guest"></div>
+      <br />
+      ${place.max_guest} Guests
+          </div>
+            <div class="number_rooms">
+            <div class="img_room"></div>
+      <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+      <br />
+      ${place.number_rooms} Bedrooms
+          </div>
+          <div class="number_bathrooms">
+          <div class="img_bathrooms"></div>
+      <br />
+      ${place.number_bathrooms} Bathroom
+          </div>
+        </div>
+      <!-- USER -->
+      <div class="user">
+      <p><b>Owner: </b>${users[place.user_id]}</p>
       </div>
-      <div class="information">
-        <div class="max_guest">
-        <div class="image_guest"></div>
-	  <br />
-	  ${place.max_guest} Guests
-	      </div>
-          <div class="number_rooms">
-          <div class="img_room"></div>
-	  <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
-	  <br />
-	  ${place.number_rooms} Bedrooms
-	      </div>
-	      <div class="number_bathrooms">
-	      <div class="img_bathrooms"></div>
-	  <br />
-	  ${place.number_bathrooms} Bathroom
-	      </div>
-	    </div>
-    <!-- USER -->
-    <div class="user">
-    <p><b>Owner: </b>${users[place.user_id]}</p>
-    </div>
-      <div class="description">
-        ${place.description}
-      </div>
-    </article> <!-- End 1 PLACE Article -->`;
-          $('section.places').append(template);
+        <div class="description">
+          ${place.description}
+        </div>
+      </article> <!-- End 1 PLACE Article -->`;
+            $('section.places').append(template);
+          }
         }
-      }
+      });
     });
   });
-});
